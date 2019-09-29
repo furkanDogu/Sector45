e3254c440a235e25a3f7a20bc4e03e1239122fc2
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-09-29 13:33:05 +0300

    Create Transaction entity

---
+++ src/entities/Transaction.ts
@@ -0,0 +1,53 @@
+import {
+    BaseEntity,
+    Entity,
+    Column,
+    ManyToOne,
+    CreateDateColumn,
+    PrimaryGeneratedColumn,
+    BeforeInsert,
+    getRepository,
+} from 'typeorm';
+import { IsNotEmpty, Min } from 'class-validator';
+
+import { Account } from '@entities';
+import { findEntityById, Lazy } from '@utils/ormHelpers';
+
+@Entity()
+export class Transaction extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    transactionNo: string;
+
+    @Min(1)
+    @Column('money')
+    amount: number;
+
+    @IsNotEmpty()
+    @ManyToOne(() => Account, account => account.transactions, { lazy: true })
+    senderAccount: Lazy<Account>;
+
+    @IsNotEmpty()
+    @ManyToOne(() => Account, account => account.transactions, { lazy: true })
+    receiverAccount: Lazy<Account>;
+
+    @CreateDateColumn()
+    date: Date;
+
+    @Column()
+    isLocal: boolean;
+
+    @BeforeInsert()
+    async setIsLocal() {
+        this.date = new Date();
+        const sender = await findEntityById(
+            getRepository(Account),
+            (await this.senderAccount).accountNo
+        );
+        const receiver = await findEntityById(
+            getRepository(Account),
+            (await this.receiverAccount).accountNo
+        );
+
+        this.isLocal = (await sender.customer).customerNo === (await receiver.customer).customerNo;
+    }
+}
