016afb600ba3a96e8e92774d49a7d80fcbbe067a
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-09-29 13:33:47 +0300

    Create migrations for all entities

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,11 @@
-export default [];
+import { Account } from './Account';
+import { Address } from './Address';
+import { Customer } from './Customer';
+import { Transaction } from './Transaction';
+
+export * from './Account';
+export * from './Address';
+export * from './Customer';
+export * from './Transaction';
+
+export default [Account, Address, Customer, Transaction];
