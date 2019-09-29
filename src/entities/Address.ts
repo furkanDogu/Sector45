b20f6ab484372dc1dea6f82da61a1ab8f488e50f
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-09-29 13:32:56 +0300

    Create Address entity

---
+++ src/entities/Address.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, OneToOne, BaseEntity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+
+import { Customer } from '@entities';
+import { Lazy } from '@utils/ormHelpers';
+
+@Entity()
+export class Address extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @IsNotEmpty()
+    @Column('varchar', { length: 255 })
+    city: string;
+
+    @IsNotEmpty()
+    @Column('varchar', { length: 255 })
+    street: string;
+
+    @IsNotEmpty()
+    @Column('varchar', { length: 255 })
+    district: string;
+
+    @IsNotEmpty()
+    @Column('varchar', { length: 255 })
+    no: number;
+
+    @OneToOne(() => Customer, customer => customer.address, { lazy: true })
+    customer: Lazy<Customer>;
+}
