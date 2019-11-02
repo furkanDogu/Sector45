ee40656e4a1d551ffe63599174a3b1dd84e5301f
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-11-02 16:11:45 +0300

    fix property count check

--- src/middlewares/bodyChecker.ts
+++ src/middlewares/bodyChecker.ts
@@ -1,10 +1,7 @@
 import { RequestHandler } from '@appTypes';
 
 export const bodyChecker = (requirements: string[]): RequestHandler<any> => (req, res, next) => {
-    if (
-        requirements.length !== Object.keys(req.body).length ||
-        !requirements.every(requirement => !!req.body[requirement])
-    ) {
+    if (!requirements.every(requirement => !!req.body[requirement])) {
         return res.send({ error: `Needed fields are: ${requirements.join(', ')}` });
     }
 
