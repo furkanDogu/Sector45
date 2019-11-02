bb3c365ca965c39c37c91b623f6ab4304517e6e0
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-11-02 15:31:23 +0300

    fix

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -5,7 +5,10 @@ import { bodyChecker } from '@middlewares';
 
 const router = Router();
 
-router.post('/register', [bodyChecker(['address']), CustomerController.register]);
+router.post('/register', [
+    bodyChecker(['TCKN', 'name', 'surname', 'password', 'salary', 'dateOfBirth', 'address']),
+    CustomerController.register,
+]);
 
 router.post('/login', [bodyChecker(['TCKN', 'password']), CustomerController.login]);
 
